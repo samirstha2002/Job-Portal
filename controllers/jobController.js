f4cce790d06ef8c461c236d9d3e6ad461d138e5e
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-11-29 17:56:02 +0545

    delete andupdate job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -5,7 +5,7 @@ const appError = require("./../utils/appError");
 exports.createJob = asyncHandler(async (req, res, next) => {
   const { company, position } = req.body;
   if (!company || !position) {
-    return next(new appError("company and position are mandatory"));
+    return next(new appError("company and position are mandatory", 400));
   }
   req.body.createdBy = req.user.userId;
   const newjob = await Job.create(req.body);
@@ -23,5 +23,47 @@ exports.getalljobs = asyncHandler(async (req, res, next) => {
     success: true,
     results: jobs.length,
     jobs,
+  });
+});
+
+exports.updatejob = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return next(new appError("company and position are mandatory", 400));
+  }
+  const job = await Job.findOne({ _id: id });
+  if (!job) {
+    return next(new appError("no jobs found with this id", 404));
+  }
+  if (req.user.userId !== job.createdBy.toString()) {
+    return next(new appError("you are not authorized to update this job", 403));
+  }
+
+  const updateJob = await Job.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    success: true,
+
+    updateJob,
+  });
+});
+exports.deletejob = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const job = await Job.findOne({ _id: id });
+  if (!job) {
+    return next(new appError("no jobs found with this id", 404));
+  }
+  if (req.user.userId !== job.createdBy.toString()) {
+    return next(new appError("you are not authorized to delete this job", 403));
+  }
+
+  await Job.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: " Job Deleted sucessfully",
   });
 });
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -3,5 +3,7 @@ const jobController = require("./../controllers/jobController");
 const router = express.Router();
 const userAuth = require("./../middlewares/authMiddleware");
 router.post("/create-job", userAuth, jobController.createJob);
-router.get("/get-job",userAuth,jobController.getalljobs)
+router.get("/get-job", userAuth, jobController.getalljobs);
+router.patch("/update-job/:id", userAuth, jobController.updatejob);
+router.delete("/delete-job/:id", userAuth, jobController.deletejob);
 module.exports = router;
