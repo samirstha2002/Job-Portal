4082d97cb1fe0a144638c04372ff1aea8a871497
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-12-06 12:13:38 +0545

    pagination filter

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -53,12 +53,22 @@ exports.getalljobs = asyncHandler(async (req, res, next) => {
   if (sort === "z-a") {
     queryResult = queryResult.sort("-position");
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  queryResult = queryResult.skip(skip).limit(limit);
+
+  const totalJobs = await Job.countDocuments(queryResult);
+  const numOfPages = Math.ceil(totalJobs / limit);
   const jobs = await queryResult;
   // const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(200).json({
     success: true,
-    results: jobs.length,
+    totalJobs,
     jobs,
+    numOfPages,
   });
 });
 
