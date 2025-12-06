3e8d05fd778d22bda9a59999424f68a0e7d06273
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-12-06 14:28:41 +0545

    swagger api document

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authController = require("./../controllers/authcontroller");
-const router = express.Router();
+
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
@@ -11,6 +11,105 @@ const limiter = rateLimit({
   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
   // store: ... , // Redis, Memcached, etc. See below.
 });
+const router = express.Router();
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: object
+ *          required:
+ *              - name
+ *              - lastName
+ *              - email
+ *              - password
+ *              - location
+ *          properties:
+ *              id:
+ *                  type: string
+ *                  description: The auto-generated id of user collection
+ *              name:
+ *                  type: string
+ *                  description: User first name
+ *              lastName:
+ *                  type: string
+ *                  description: User last name
+ *              email:
+ *                  type: string
+ *                  format: email
+ *                  description: User email address
+ *              password:
+ *                  type: string
+ *                  minLength: 6
+ *                  description: User password (at least 6 characters)
+ *              location:
+ *                  type: string
+ *                  description: User location (city or country)
+ *          example:
+ *              id: "Ghhh667jkl"
+ *              name: "Samir"
+ *              lastName: "Shrestha"
+ *              email: "samirs@gmail.com"
+ *              password: "sam12345"
+ *              location: "Nepal"
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Auth
+ *  description: Authentication apis
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *   post:
+ *     summary: Register new user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *   post:
+ *     summary: login user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User login successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: something went wrong
+ */
+
 router.post("/register", limiter, authController.registerUser);
 router.post("/login", authController.loginUser);
 module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
-
-const xss=require('xss-sanitize')
-const mongoSanitizer= require('express-mongo-sanitize')
+const swaggerDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const xss = require("xss-sanitize");
+const mongoSanitizer = require("express-mongo-sanitize");
 
 // files
 const dbconnect = require("./config/db");
@@ -15,21 +16,45 @@ const authRouter = require("./routes/authRoutes");
 const errorHandler = require("./middlewares/errorhandler");
 const userRouter = require("./routes/userRoutes");
 const jobRouter = require("./routes/jobRoutes");
+
+//database connection
 dbconnect();
 
+// swagger api config
+// swagger api options
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Job Portal Application",
+      description: "Node Js Express Job Portal Application",
+    },
+    servers: [
+      {
+        url: "http://127.0.0.1:8000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const spec = swaggerDoc(options);
 const app = express();
 
 //middlewaresa
 app.use(helmet());
+app.use(cors({ origin: "*" })); // allow all origins
 app.use(express.json());
-app.use(cors());
 app.use(morgan("dev"));
+app.use(xss());
+app.use(mongoSanitizer());
 
 //routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
-
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
 app.use(errorHandler);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
